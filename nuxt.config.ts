fd136ffcce6ba1b5e188e9230b4491136b9139fd
Alejandro Macias <alex.mac.fon@gmail.com>
2025-12-01 11:42:50 -0600

    fix: transpile barcode-reader into js code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ['vue-barcode-reader']
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/tailwind.css"],
